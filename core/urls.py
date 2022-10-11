4e0297033e26d635e18329edfa6143c5951a9dd4
Dinara <dineirashafigulllina@gmail.com>
2022-10-11 15:39:23 +0500

    make class_based views

--- core/urls.py
+++ core/urls.py
@@ -1,19 +1,19 @@
-from django.urls import path, include
+from django.urls import path
 
 from core.views import *
 
 app_name = 'core'
 urlpatterns = [
-    path('', index),
-    path('incidents/', number_of_incidents, name='incidents'),
-    path('telephone/<int:pk>/', telephone, name='telephone'),
-    path('<int:pk>/', redirected,name='redir'),
-    path('inf/', inf, name='inf'),
-    path('applic/', applic_dict, name='applic'),
-    path('applicJ/<int:pk>/', applic_json, name='applicJ'),
-    path('appeal/', appeal, name='appeal'),
-    path('emergency/', emergency, name='emergency'),
-    path('applicant/', applicant, name='applicant'),
-    path('a_list/', applicant_list, name='applicant_list'),
-    path('appeal_list/', appeal_list, name='appeal_list')
+    path('', Index.as_view()),
+    path('incidents/', NumberOfIncidents.as_view(), name='incidents'),
+    path('telephone/<int:pk>/', Telephone.as_view(), name='telephone'),
+    path('<int:pk>/', Redir.as_view(),name='redir'),
+    path('inf/', Inf.as_view(), name='inf'),
+    path('applic/', ApplicDict.as_view(), name='applic'),
+    path('applicJ/<int:pk>/', ApplicJSON.as_view(), name='applicJ'),
+    path('appeal/', AppealView.as_view(), name='appeal'),
+    path('emergency/', EmergencyView.as_view(), name='emergency'),
+    path('applicant/', ApplicantView.as_view(), name='applicant'),
+    path('a_list/', ApplicantList.as_view(), name='applicant_list'),
+    path('appeal_list/', AppealList.as_view(), name='appeal_list')
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,5 @@
 import datetime
-from multiprocessing import context
-import re
+from django.views.generic import View
 from django.shortcuts import get_object_or_404, get_list_or_404, render
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from core.models import *
@@ -11,45 +10,45 @@ menu =[{'title':'Обращения','url_name': 'core:appeal'},
     {'title':'Заявители','url_name': 'core:applicant'},
     {'title':'Службы','url_name': 'core:emergency'}]
 
-
-def number_of_incidents(request):
-    incident = get_list_or_404(Appeal.objects)
-    return HttpResponse("Количество происшествий - %s" % len(incident))
-
-
-
-def telephone(request,pk):
-    telephone = get_object_or_404(Applicant.objects.values_list('telephone',), id__iexact=pk)
-    return HttpResponse(telephone)
+class NumberOfIncidents(View):
+    def get(self, request):
+        incident = get_list_or_404(Appeal.objects)
+        return HttpResponse("Количество происшествий - %s" % len(incident))
 
 
-def redirected(request,pk):
-    return HttpResponseRedirect('/incidents/')
+class Telephone(View):
+    def get(self,request,pk):
+        telephone = get_object_or_404(Applicant.objects.values_list('telephone',), id__iexact=pk)
+        return HttpResponse(telephone)
 
+class Redir(View):
+    def get(self,request,pk):
+        return HttpResponseRedirect('/incidents/')
 
-def inf(request):
-    if request.GET:
-        return HttpResponse(list(request.GET.dict().items()))
+class Inf(View):
+    def get(self, request):
+        if request.GET:
+            return HttpResponse(list(request.GET.dict().items()))
 
+class ApplicDict(View):
+    def get(self,request):
+        tel = int(request.GET.get('tel'))
+        applicant = Applicant.objects.filter(telephone=tel).values()
+        return HttpResponse(applicant)
+class ApplicJSON(View):
+    def get(self,request,pk):
+        applicant = get_object_or_404(Applicant, id=pk)
+        data = model_to_dict(applicant, exclude='image')
+        return JsonResponse(data)
 
-def applic_dict(request):
-    tel = int(request.GET.get('tel'))
-    applicant = Applicant.objects.filter(telephone=tel).values()
-    return HttpResponse(applicant)
-
-def applic_json(request,pk):
-    applicant = get_object_or_404(Applicant, id=pk)
-    data = model_to_dict(applicant, exclude='image')
-    return JsonResponse(data)
-
-
-def appeal(request):
-    appeals = Appeal.objects.all()
-    avg_em = Appeal.objects.aggregate(Avg('emergency'))
-    sublist = Appeal.objects.values_list('number', 'emergency__name')
-    count_appeal = Appeal.objects.all().count()
-    now = datetime.datetime.now()
-    context = {
+class AppealView(View):
+    def get(self, request):
+        appeals = Appeal.objects.all()
+        avg_em = Appeal.objects.aggregate(Avg('emergency'))
+        sublist = Appeal.objects.values_list('number', 'emergency__name')
+        count_appeal = Appeal.objects.all().count()
+        now = datetime.datetime.now()
+        context = {
         'menu': menu,
         'title' : 'Обращения',
         'appeals': appeals, 
@@ -57,55 +56,57 @@ def appeal(request):
         'sublist': sublist,
         'count_appeal': count_appeal,
         'now' : now
-    }
-    return render(request, 'core/appeal.html', context=context)
+        }
+        return render(request, 'core/appeal.html', context=context)
 
-
-def applicant(request):
-    applicants = Applicant.objects.all()
-    context = {
+class ApplicantView(View):
+    def get(self, request):
+        applicants = Applicant.objects.all()
+        context = {
         'menu': menu,
         'title': 'Заявители', 
         'applicants': applicants
-    }
-    return render(request, 'core/application.html', context=context)
+        }
+        return render(request, 'core/application.html', context=context)
 
-def emergency(request):
-    emergencies = Emergency.objects.all()
-    context = {
+class EmergencyView(View):
+    def get(self, request):
+        emergencies = Emergency.objects.all()
+        context = {
         'menu': menu,
         'title':'Службы', 
         'emergencies': emergencies
-    }
-    return render(request, 'core/emergency.html', context=context)   
+        }
+        return render(request, 'core/emergency.html', context=context)   
 
-def index(request):
-    context = {
+class Index(View):
+    def get(self,request):
+        context = {
         'menu': menu,
         'title': 'Главная страница'
-    }
-    return render(request,'core/index.html', context=context)
+        }
+        return render(request,'core/index.html', context=context)
 
-def applicant_list(request):
-    applicants_list = Applicant.objects.all()
-    context = {
+class ApplicantList(View):
+    def get(self,request):
+        applicants_list = Applicant.objects.all()
+        context = {
         'menu': menu,
         'title': 'Список заявителей', 
         'applicant_list': applicants_list
-    }
-    
-    return render(request, 'core/applicant_list.html', context=context)
+        }
+        return render(request, 'core/applicant_list.html', context=context)
 
-def appeal_list(request):
-    appeal_list = Appeal.objects.all()
-    sublist = Appeal.objects.values_list('number', 'emergency__name')
-    print(appeal_list)
-    print(sublist)
-    context = {
+class AppealList(View):
+    def get(self,request):
+        appeal_list = Appeal.objects.all()
+        sublist = Appeal.objects.values_list('number', 'emergency__name')
+        print(appeal_list)
+        print(sublist)
+        context = {
         'menu': menu,
         'title': 'Список происшествий', 
         'appeal_list': appeal_list,
         'sublist': sublist
-    }
-    
-    return render(request, 'core/appeal_list.html', context=context)
+        } 
+        return render(request, 'core/appeal_list.html', context=context)
